5a20b9ded1bca9a1518bc0d87acf11084a537e94
Solomon  Ifeoluwa <solomite99@gmail.com>
2026-05-26 23:08:04 +0100

    add terraform

--- main.tf
+++ main.tf
@@ -93,3 +93,222 @@ resource "aws_route_table_association" "private_2_assoc" {
   subnet_id      = aws_subnet.private_2.id
   route_table_id = aws_route_table.private.id
 }
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  description = "Allow HTTP traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "ec2_sg" {
+  name   = "backend-security-group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "redis_sg" {
+  name   = "redis-security-group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_iam_role" "ec2_role" {
+  name = "starttech-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "starttech-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+resource "aws_launch_template" "backend_template" {
+  name_prefix   = "starttech-backend"
+  image_id      = "ami-05d62b9bc5a6ca605"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_profile.name
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+
+              docker run -d -p 8080:8080 yourdockerhubusername/starttech-backend
+              EOF
+  )
+}
+resource "aws_lb_target_group" "backend_tg" {
+  name     = "backend-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path = "/health"
+    port = "8080"
+  }
+}
+resource "aws_lb" "backend_alb" {
+  name               = "starttech-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.alb_sg.id
+  ]
+
+  subnets = [
+    aws_subnet.public.id
+  ]
+}
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+  }
+}
+resource "aws_autoscaling_group" "backend_asg" {
+  desired_capacity = 2
+  max_size         = 4
+  min_size         = 2
+
+  vpc_zone_identifier = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+
+  target_group_arns = [
+    aws_lb_target_group.backend_tg.arn
+  ]
+
+  launch_template {
+    id      = aws_launch_template.backend_template.id
+    version = "$Latest"
+  }
+
+  health_check_type = "EC2"
+}
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "starttech-frontend-bucket-040899"
+}
+resource "aws_s3_bucket_website_configuration" "frontend_website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+resource "aws_cloudfront_distribution" "frontend_cdn" {
+  enabled = true
+
+  origin {
+    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_id   = "frontendS3"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "frontendS3"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "starttech-redis"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  port                 = 6379
+
+  security_group_ids = [
+    aws_security_group.redis_sg.id
+  ]
+}
